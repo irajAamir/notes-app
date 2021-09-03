b996e346aff0992a9dcf8f1e0185ecc189cafc2a
Hassan Azhar Khan <hassanazharkhan@gmail.com>
2021-09-03 23:36:37 +0500

    Clean out the file structure

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const chalk = require("chalk")
 const yargs = require("yargs")
-const getNotes = require('./notes.js')
-// //console.log(process.argv)
-// console.log(yargs.agrv)
+//const getNotes = require('./notes.js')
 
-// const command = process.argv[2]
-//  if(command === "add"){
-//      console.log("adding")
-//  }else if(command === "remove"){
-//      console.log("removing")
-//  }
+const command = process.argv[2]
+console.log(command)
+ if(command === "add"){
+     console.log("adding")
+ }else if(command === "remove"){
+     console.log("removing")
+ }
  console.log(process.argv)
- console.log(yargs.agrv)
